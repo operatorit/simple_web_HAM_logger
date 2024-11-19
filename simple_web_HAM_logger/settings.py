fce95d972eddcb86352861cd7ece8737ea74cdde
operatorit <ariaad@gmail.com>
2024-11-19 23:13:40 +0100

    Packages versions upgraded in requirements.txt

--- simple_web_HAM_logger/settings.py
+++ simple_web_HAM_logger/settings.py
@@ -43,7 +43,7 @@ elif os.environ.get('DEBUG') == 'FALSE':
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
